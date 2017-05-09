5f645921c970271b950e6e38e6a08baaec67bb7d
liangxiaoyu <liangxiaoyu828@gmail.com>
2017-05-09 15:13:51 -0700

    change pho size

--- app/src/main/java/com/example/xiaoyuliang/foodie/Cover.java
+++ app/src/main/java/com/example/xiaoyuliang/foodie/Cover.java
@@ -68,7 +68,7 @@ public class Cover extends AppIntro {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_intro2, container, false);
+            return inflater.inflate(R.layout.fragment2, container, false);
         }
 
     }
@@ -78,7 +78,7 @@ public class Cover extends AppIntro {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_intro3, container, false);
+            return inflater.inflate(R.layout.fragment3, container, false);
         }
     }
 }
